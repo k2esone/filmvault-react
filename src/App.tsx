1bbe33a6a10ab96f5bec00e6973ba486fbe869d8
Mateusz <wmateusz993@gmail.com>
2023-05-28 08:01:23 +0200

    New navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 
 import './App.css';
 import BrandLogo from './components/BrandLogo';
+import Carousel from './components/Carousel';
+import MovieComp from './components/MovieComp';
 import NavBar from './components/NavBar';
 import Header from './pages/Home/Header';
+import TopMovies from './pages/Home/TopMovies';
 
 function App() {
   return (
-  <>
-  <NavBar></NavBar>
-  <Header></Header>
-  </>
-  )
+ <>
+ <NavBar></NavBar>
+<Header></Header>
+ </>
+  );
 }
 
 export default App;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,39 +1,49 @@
+import { useState } from "react";
 import "./NavBar.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 const NavBar = () => {
-	return (
-		<nav>
+	const [active, setActive] = useState(false);
+
+	const handleMouseOver = () => {
+		setActive(true);
+	};
+	const handleMouseOut = () =>{
+		setActive(false)
+	}
 	
-			<div className="nav_items">
-      <button className="burger_btn">
-				<div className="burger_btn_box">
-					<div className="burger_btn_bars"></div>
+
+
+	return (
+		<nav id="navbar" className="navbar navbar-expand-lg position-sticky top-0">
+			<div className="container">
+				<a className="navbar-brand" href="#">
+					<i className="fa-solid fa-film"></i> FilmVALUT
+				</a>
+				<button
+				onMouseOver={handleMouseOver}
+				onMouseOut ={handleMouseOut}
+
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNavAltMarkup"
+					aria-controls="navbarNavAltMarkup"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<i className="fas fa-bars"></i>
+				</button>
+				<div className={active? 'collapse navbar-collapse show':"collapse navbar-collapse"} id="navbarNavAltMarkup">
+					<div className="navbar-nav ms-auto">
+						<a className="nav-link" href="#">
+							LogIn
+						</a>
+						<a className="nav-link" href="#aboutus">
+							Register
+						</a>
+						<a className="nav-link" href="#portfolio"></a>
+					</div>
 				</div>
-			</button>
-				<div className="log">
-					<h2>FilmValut</h2>
-				</div>
-				<div className="menu_item one">
-					Menu
-          </div>
-          <button className="burger_btn">
-				<div className="burger_btn_box">
-					<div className="burger_btn_bars"></div>
-				</div>
-			</button>
-					<ul className="dropdown">
-						<li>
-							<a href="#">LogIn</a>
-						</li>
-						<li>
-							<a href="#">Register</a>
-						</li>
-						<li>
-							<a href="#">Contact</a>
-						</li>
-					</ul>
-				</div>
-			
+			</div>
 		</nav>
 	);
 };
